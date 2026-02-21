9efc9693df2c35d813a98aed6eb9afe3107a8116
Aashish Kumar Singh <aashishsssindustries@gmail.com>
2026-02-21 18:43:20 +0530

    feat: -/book api removed -implemented inventory/reserve api - implemented inventory/release api - Tested api endpoints manually using postman and verified the results - Locking still working

--- flash-tix/inventory-service/app/main.py
+++ flash-tix/inventory-service/app/main.py
@@ -3,42 +3,68 @@ from fastapi import FastAPI
 from app.database import engine,Base
 from app.models.event import Event
 from fastapi import HTTPException
-
+from pydantic import BaseModel
 app=FastAPI()
+
+class ReserveRequest(BaseModel):
+    event_id: int
+    quantity: int=1
 
 @app.on_event("startup")
 def startup():
     Base.metadata.create_all(bind=engine)
+
 @app.get("/health")
 def health_check():
     return {"status":"ok"}
 
-@app.post("/book/{event_id}")
-def book_ticket(event_id: int):
+@app.post("/inventory/reserve")
+def reserve_ticket(request:ReserveRequest):
     db=SessionLocal()
-    
+
+    try:
+        event=(db.query(Event).filter(Event.id==request.event_id)
+        .with_for_update().first()
+        )
+        if not event:
+            raise HTTPException(status_code=404,detail="Event not found")
+        
+        if event.available_tickets<request.quantity:
+            raise HTTPException(status_code=400,detail="Not enough tickets")
+        
+        event.available_tickets -= request.quantity
+        db.commit()
+
+        return{
+            "status":"RESERVED",
+            "remaining_tickets":event.available_tickets
+        }
+
+    finally:
+        db.close()
+
+@app.post("/inventory/release")
+def release_ticket(request:ReserveRequest):
+    db=SessionLocal()
+
     try:
         event=(
             db.query(Event)
-            .filter(Event.id==event_id)
+            .filter(Event.id==request.event_id)
             .with_for_update()
             .first()
         )
 
         if not event:
-            raise HTTPException(status_code=404,detail="event not found")
-        
-        if event.available_tickets<=0:
-            raise HTTPException(status_code=400,detail="Sold out")
+            raise HTTPException(status_code=404,detail="Event not found")
 
-        event.available_tickets-=1
+        event.available_tickets+=request.quantity
         db.commit()
 
-        remaining=event.available_tickets
-
         return{
-            "message":"Ticket booked successfully",
-            "remaining_tickets":remaining
+            "status":"RELEASED",
+            "available_tickets":event.available_tickets
         }
+
     finally:
         db.close()
