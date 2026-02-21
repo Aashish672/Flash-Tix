a3b8263c0ff129fd793b3391991eb0a45429c828
Aashish Kumar Singh <aashishsssindustries@gmail.com>
2026-02-21 23:09:58 +0530

    feat: - /orders endpoint implemented - DB verified

--- flash-tix/order-service/app/main.py
+++ flash-tix/order-service/app/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from app.database import engine,Base
 from app.models.order import Order
+from fastapi import HTTPException
+from app.database import SessionLocal
+import requests
+
+from pydantic import BaseModel
+
+class CreateOrderRequest(BaseModel):
+    event_id:int
+    quantity:int=1
 
 app=FastAPI()
 
@@ -11,3 +20,39 @@ def startup():
 @app.get("/health")
 def health_check():
     return {"status":"ok"}
+
+@app.post("/orders")
+def create_order(request: CreateOrderRequest):
+    db = SessionLocal()
+
+    try:
+        # Call Inventory Service
+        response = requests.post(
+            "http://inventory-service:8000/inventory/reserve",
+            json={
+                "event_id": request.event_id,
+                "quantity": request.quantity
+            }
+        )
+
+        if response.status_code != 200:
+            raise HTTPException(status_code=400, detail="Inventory reservation failed")
+
+        # Create order
+        order = Order(
+            event_id=request.event_id,
+            quantity=request.quantity,
+            status="CREATED"
+        )
+
+        db.add(order)
+        db.commit()
+        db.refresh(order)
+
+        return {
+            "order_id": order.id,
+            "status": order.status
+        }
+
+    finally:
+        db.close()
