9aa2ab0bfdd0c529ea66800f9ae6bdaa362c6df2
Aashish Kumar Singh <aashishsssindustries@gmail.com>
2026-02-24 19:27:38 +0530

    feat: Implement initial payment service with FastAPI, SQLAlchemy, and Docker Compose integration.

--- flash-tix/payment-service/app/main.py
+++ flash-tix/payment-service/app/main.py
@@ -1,7 +1,48 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from app.database import engine, Base, SessionLocal
+from app.models.payment import Payment
+from pydantic import BaseModel
+import random
 
 app=FastAPI()
+
+class PaymentRequest(BaseModel):
+    order_id: int
+    amount: int
+
+@app.on_event("startup")
+def startup():
+    Base.metadata.create_all(bind=engine)
 
 @app.get("/health")
 def health_check():
     return {"status":"ok"}
+
+@app.post("/payments")
+def process_payment(request:PaymentRequest):
+    db=SessionLocal()
+
+    try:
+        #Simulate Random Failure
+        success=random.choice([True,True,True,False]) # 75% Success
+
+        payment=Payment(
+            order_id=request.order_id,
+            amount=request.amount,
+            status="success" if success else "failed"
+        )
+
+        db.add(payment)
+        db.commit()
+        db.refresh(payment)
+
+        if not success:
+            raise HTTPException(status_code=400,detail="Payment Failed")
+
+        return {
+            "payment_id":payment.id,
+            "status":payment.status
+        }
+    
+    finally:
+        db.close()
---
+++ flash-tix/payment-service/app/models/payment.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.sql import func
+from app.database import Base
+
+class Payment(Base):
+    __tablename__="payments"
+
+    id=Column(Integer,primary_key=True,index=True)
+    order_id=Column(Integer,nullable=False)
+
+    amount=Column(Integer,nullable=False)
+    status=Column(String,nullable=False)
+
+    created_at=Column(DateTime(timezone=True),server_default=func.now())
